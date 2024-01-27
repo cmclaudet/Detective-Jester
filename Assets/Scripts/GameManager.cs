66bf431a04d7548f182be1374c629b0458b91e0a
Sandroleon313 <sandroflei313@gmail.com>
2024-01-27 15:57:12 +0100

    Simple GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    public DialogueRunner dr;
+    public PlayerInput pi;
+    private int lives = 2;
+    private int laughts = 0;
+
+
+    private void Awake()
+    {
+        dr.AddCommandHandler<int>(
+            "loseLife",
+            loseLife
+        );
+        dr.AddCommandHandler<int>(
+            "getLaught",
+            getLaugh
+        );
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Start Initial Dialogue
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("g"))
+        {
+            phase2();
+        }
+    }
+
+    public void phase2()
+    {
+        pi.DeactivateInput();
+        dr.StartDialogue("Phase2");
+        
+    }
+
+
+
+    public void loseLife(int x)
+    {
+        lives -= x;
+        if(lives <= 0)
+        {
+            badEnding();
+        }
+    }
+
+    public void getLaugh(int x)
+    {
+        laughts += x;
+        if(laughts >= 5)
+        {
+            goodEnding();
+        }
+    }
+
+    private void badEnding()
+    {
+        dr.Stop();
+        dr.StartDialogue("BadEnding");
+    }
+
+    private void goodEnding()
+    {
+        dr.Stop();
+        dr.StartDialogue("GoodEnding");
+    }
+    
+    
+}
